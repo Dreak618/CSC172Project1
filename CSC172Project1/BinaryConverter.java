81ba3cc7560ea6506862bfebb18632ccb43d6050
Dreak618 <nkrein@u.rochester.edu>
2024-02-27 13:52:41 -0500

    added file for binary converter, to be implemented

---
+++ CSC172Project1/BinaryConverter.java
@@ -0,0 +1,13 @@
+
+public class BinaryConverter{
+
+    public BinaryConverter(){ // Constructor needed?
+
+    }
+    public static long toBinary(String s){ // given input string
+        return 0; // for now
+    }
+    public static String toPlainText(long l){ // given binary
+        return ""; // for now
+    }
+}
