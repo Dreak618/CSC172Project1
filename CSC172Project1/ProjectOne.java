2a98984d043515664923ef04b87dafbff4bc9537
Benjamin Levy <benjaminlevy@dhcp-10-5-155-231.wireless.rochester.edu>
2024-02-27 12:24:58 -0500

    update1 - started filewriters

--- CSC172Project1/ProjectOne.java
+++ CSC172Project1/ProjectOne.java
@@ -1,15 +1,19 @@
+
 //Nicholas Krein / Benjamin Levy 
-import java.io.*; //File, reader, writer
+//File, reader, writer
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 // REMEBER TO REMOVE PACKAGES BEFORE SUBMISSION
 public class ProjectOne {
+    private String inputFilePath, encryptPath, decryptPath;
+
     public static void main(String[] args) throws FileNotFoundException, IOException {
         // Test cases here
         tests.runTests();
-
-
-
 
         Scanner s = new Scanner(System.in);
         System.out.print("Do you want to encrypt or decrypt (E/D): ");
@@ -19,12 +23,40 @@ public class ProjectOne {
         System.out.println("Output file: ");
         String outputFileName = s.nextLine();
         File outputFile = new File(outputFileName);
-        //FileReader inputReader = new FileReader(inputFileName);
+        // FileReader inputReader = new FileReader(inputFileName);
         FileWriter fw = new FileWriter(outputFileName);
-        //doEncrypt()
-        //printf("Secret key: %s \n", secretKey);
-        //printf("Output file: %s", outputFileName)
+        // doEncrypt()
+        // printf("Secret key: %s \n", secretKey);
+        // printf("Output file: %s", outputFileName)
     }
 
+    public ProjectOne(String filePath) {
+        inputFilePath = filePath;
+        Encrypter encrypter = new Encrypter();
+        Decrypter decrypter = new Decrypter();
+    }
 
+    protected class Encrypter {
+        protected Encrypter() {
+            createEncryptedFile();
+        }
+
+        public void createEncryptedFile() {
+            File encryptedFile = new File("encrypted." + inputFilePath);
+            encryptPath = encryptedFile.getAbsolutePath();
+        }
+
+    }
+
+    protected class Decrypter {
+        protected Decrypter() {
+            createDecryptedFile();
+        }
+
+        protected void createDecryptedFile() {
+            File decryptedFile = new File("decrypted." + inputFilePath);
+            encryptPath = decryptedFile.getAbsolutePath();
+        }
+
+    }
 }
