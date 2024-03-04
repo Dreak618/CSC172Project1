06ba727d3846b6efd3a9fb60d037bdeb38bd9258
Ben Levy <imbenjaminlevy@gmail.com>
2024-03-04 18:58:02 -0500

    fixed swapping

--- CSC172Project1/Decrypter.java
+++ CSC172Project1/Decrypter.java
@@ -72,24 +72,19 @@ public class Decrypter {
                 String L = split[0];
                 String R = split[1];
                 String temp; // temp variable used to swap L,R halves after each iteration
-                // temp = L; // swap L, R
-                // L = R;
-                // R = temp;
-                // for (int i = 10; i > 0; i--) {
-                // temp = L; // swap L, R
-                // L = R;
-                // R = temp;
+                for (int i = 10; i > 0; i--) {
 
-                R = xorItUndo(R, L);
+                        R = xorItUndo(R, L);
 
-                inputKey = keyScheduleTransform(inputKey);
-                R = functionF(R, inputKey);
-                // }
-                temp = L; // swap L, R
-                L = R;
-                R = temp;
+                        inputKey = keyScheduleTransform(inputKey);
+                        R = functionF(R, inputKey);
+                        temp = L; // swap L, R
+                        L = R;
+                        R = temp;
+                }
 
                 return R + L;
+
         }
 
         // converts binary into plain-text
